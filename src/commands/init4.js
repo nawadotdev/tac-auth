7f960e97ba34706534d376e4fdc141963c141280
devnawa <148815756+devnawa@users.noreply.github.com>
2025-06-04 15:31:50 +0300

    text change

--- src/commands/init4.js
+++ src/commands/init4.js
@@ -21,7 +21,7 @@ export default {
                     new EmbedBuilder()
                         .setColor(0x0099ff)
                         .setTitle("TAC Force Board")
-                        .setDescription("Please click on the ‘Check Rank’ button below to know your rank in the TAC Force Board.\n\nNot all Discord activities earn ‘Snaps’. Read this for more #info about the TAC Snaps initiative.")
+                        .setDescription("Please click on the ‘Check Rank’ button below to check your rank in the TAC Force Board.\n\nNot all Discord activities earn ‘Snaps’. Read the <#1379084827914928220> channel to learn more about the TAC Snaps initiative.\n\nSnaps are awarded on a 24-48 hour basis and hence you don’t have to check it every hour.")
                         .setFooter({ text: "TAC.build ~ nawadotdev"})
                 ],
                 components: [row]
